8c043aa7e26793394c7b9094c5cf79b8d9accc29
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-06-07 23:18:19 -0500

    coming soon plus image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
+import './App.css'
+
 export default function App() {
-  return (
-    <div>UNDER CONSTRUCTION</div>
-  )
+    return (
+        <div className='App'>
+            <h1>COMING <br /> SOON...</h1>
+            <img src='https://cdn4.iconfinder.com/data/icons/casino-general/512/777-512.png' alt='coming soon' height={100} width={100} />
+        </div>
+    )
 }
